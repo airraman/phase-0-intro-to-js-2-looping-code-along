03d8e9479634a7e16909df0f5d29930bf8288530
Raman <ramanmcreates@gmail.com>
2022-09-21 10:38:30 -0400

    commit tree f5b1941f6ec3165a0554de0518ff0e22d7bbc29a

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solutions in this file
+function writeCards(names, eventName){
+
+    
+
+for( let i = 0; i < names.length; i++){
+    return `Thank you ${names[i]} for the wonderful ${eventName} gift`
+}
+}
+eventName = "wedding"
+
+writeCards(names = ["Raman","Dylan", "Tor"], eventName)
